59def205762527188d84189dd3e371e64e92a8c6
Gregory N <gregoryn@devexpress.com>
2024-07-09 03:49:45 -0700

    update authOptions

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,6 +1,6 @@
 import SignInForm from "@/components/SignInForm";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+import authOptions from "@/lib/authOptions";
 import { redirect } from "next/navigation";
 
 const SignInPage = async () => {
